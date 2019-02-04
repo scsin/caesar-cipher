19ac9fdf9b81be6e7512a55f51e52d02bd53f620
Sara Sin <saracsin26@gmail.com>
2019-02-03 23:52:33 -0200

    Add files via upload

--- src/tests.js
+++ src/tests.js
@@ -1,8 +1,3 @@
-/* Hacker edition
-   Permite usar um offset (deslocamento) negativo
-   Cifra/descifra outros caracteres (espaços, pontuação, ã, á, ...)
-   + py
-*/
 let str;
 let chav;
 let strArr;
@@ -27,7 +22,7 @@ function buttonD() {
   decode(strArr, chav);
 }
 
-// document.write(`A divisão da palavra é: ${strArr} `);
+// document.write(`${strArr} `);
 
 // alphabet
 function alphOrg() {
@@ -52,10 +47,6 @@ function alphDesloc() {
 
 // adicionar o espaço = ASC(20) nos alfabetos;
 
-
-// encode
-// fazer um if ou um botão (para if (id = 'encode' então .....) if ou else (id = 'decode' então ....))
-// if (chav > 0) {  IF E ELSE PARA NÚMERO NEGATIVO (se colocar esse bloco precisa fazer + blocos para a montagem do alfEnc)
 function encode() {
   for (a = 0; a <= strArr.length; a++) {
     for(i = 65; i <= 90; i++) {
@@ -66,30 +57,17 @@ function encode() {
       }
     }
   }
+  // teste
+  if (arrEnc.join('') == 'XYZ') {
+    console.log('correto');
+  }
+  else {
+    console.log('erro');
+  }
 }
 
 
-// }
-// else {
-//   for (a = 0; a <= strArr.length; a++) {
-//     for(i = 65; i <= 90; i++) {
-//       if (strArr[a] == String.fromCharCode(i)) {
-//         var pos = ((i-65)-chav);
-//         arrEnc.push(alfEnc[pos]);
-//         console.log('Para a palavra: ' + strArr[a] + ' a cifra é: ' + alfEnc[pos]);
-//       }
-//     }
-//   }
-// }
-// teste
-// if (arrEnc.join('') == 'XYZ') {
-//   console.log('legal');
-// }
-// else {
-//   console.log('erro');
-// }
 
-// decode
 function decode() {
   for (a = 0; a <= strArr.length; a++) {
     for(i = 0; i <= 25; i++) {
@@ -99,5 +77,12 @@ function decode() {
         document.getElementById("decode").innerHTML = arrEnc.join('');
       }
     }
+  }
+  // teste
+  if (arrEnc.join('') == 'ABC') {
+    console.log('correto');
+  }
+  else {
+    console.log('erro');
   }
 }
